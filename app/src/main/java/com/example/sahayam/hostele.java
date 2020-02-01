5efdcba0c908a036b117248e9bc3e17a0b5062d4
miniam <malladisiddhu@gmail.com>
2020-02-01 08:35:15 +0530

    third update

--- app/src/main/java/com/example/sahayam/hostele.java
+++ app/src/main/java/com/example/sahayam/hostele.java
@@ -10,7 +10,7 @@ public class hostele extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_hostele);
 
 
         VideoView videoView = findViewById(R.id.video_view);
